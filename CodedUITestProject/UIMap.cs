290f3bd7b8eb20d93c466882a052784d381cb4a9
wdemey <jdoe7435@gmail.com>
2015-06-18 01:43:45 +0200

    Check in

---
+++ CodedUITestProject/UIMap.cs
@@ -0,0 +1,43 @@
+﻿namespace CodedUITestProject
+{
+    using System;
+    using System.Collections.Generic;
+    using System.CodeDom.Compiler;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
+    
+    public partial class UIMap
+    {
+        public void ModifiedSimpleAppTest()
+        {
+            #region Variable Declarations
+            WpfButton uIStartButton = this.UIMainWindowWindow.UIStartButton;
+            WpfCheckBox uICheckBoxCheckBox = this.UIMainWindowWindow.UICheckBoxCheckBox;
+            WpfButton uICloseButton = this.UIMainWindowWindow.UIMainWindowTitleBar.UICloseButton;
+            #endregion
+
+            // Launch '%VisualStudioDir%\Projects\DemoApp\DemoApp\bin\Debug\DemoApp.exe'
+            ApplicationUnderTest uIMainWindowWindow = ApplicationUnderTest.Launch(this.SimpleAppTestParams.UIMainWindowWindowExePath, this.SimpleAppTestParams.UIMainWindowWindowAlternateExePath);
+
+            // Click 'Start' button
+            Mouse.Click(uIStartButton, new Point(62, 22));
+
+            uICheckBoxCheckBox.WaitForControlEnabled();
+
+           
+            // Select 'CheckBox' check box
+            uICheckBoxCheckBox.Checked = this.SimpleAppTestParams.UICheckBoxCheckBoxChecked;
+
+            // Click 'Close' button
+            Mouse.Click(uICloseButton, new Point(14, 6));
+        }
+    }
+}
